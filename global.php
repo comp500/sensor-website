90465f1be6799983151f052614e3ef32a5b11f2c
comp500 <comp500@users.noreply.github.com>
2017-06-14 16:58:53 +0100

    use memcache for smarty caching

--- global.php
+++ global.php
@@ -9,9 +9,10 @@ $smarty->setConfigDir(__DIR__ . '/smarty/configs/');
 if (!isset($_SERVER['DEFAULT_VERSION_HOSTNAME'])) { // check if running on google cloud
 	$smarty->setCompileDir(__DIR__ . '/smarty/templates_c/');
 	$smarty->setCacheDir(__DIR__ . '/smarty/cache/');
-} else { // use cloud storage for compile dirs
-	$smarty->setCompileDir('gs://' . $_SERVER['DEFAULT_VERSION_HOSTNAME'] . '/smarty_tmp/templates_c/');
-	$smarty->setCacheDir('gs://' . $_SERVER['DEFAULT_VERSION_HOSTNAME'] . '/smarty_tmp/cache/');
+} else { // use memcached for compile dirs
+	require_once 'memcachedSmarty.php';
+	$smarty->registerCacheResource('memcache', new Smarty_CacheResource_Memcache());
+	$smarty->caching_type = 'memcache';
 }
 
 ?>
