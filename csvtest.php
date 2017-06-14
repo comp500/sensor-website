89ff1ef4a9b530e0390cc1a080b454a49bd6e7ef
comp500 <comp500@users.noreply.github.com>
2017-06-14 18:31:12 +0100

    php OOP is great

--- csvtest.php
+++ csvtest.php
@@ -2,6 +2,6 @@
 require 'global.php';
 
 foreach($obj_store->fetchAll() as $obj_book) {
-	$data = $obj_book.getData();
+	$data = $obj_book->getData();
     echo "Temperature: {$data['0']}, Recorded: {$data['recorded']} <br />", PHP_EOL;
 }
