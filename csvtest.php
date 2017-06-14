03c1b238f929c8de924a36f9322cea6381f2df4e
comp500 <comp500@users.noreply.github.com>
2017-06-14 18:26:23 +0100

    get data array

--- csvtest.php
+++ csvtest.php
@@ -2,5 +2,6 @@
 require 'global.php';
 
 foreach($obj_store->fetchAll() as $obj_book) {
-    echo "Temperature: {$obj_book['0']}, Recorded: {$obj_book->recorded} <br />", PHP_EOL;
+	$data = $obj_book.getData();
+    echo "Temperature: {$data['0']}, Recorded: {$data['recorded']} <br />", PHP_EOL;
 }
