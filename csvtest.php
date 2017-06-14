b51780bf4f05ad87ff51f2d39183c2ce5f6e540f
comp500 <comp500@users.noreply.github.com>
2017-06-14 18:16:20 +0100

    fix csvtest?

--- csvtest.php
+++ csvtest.php
@@ -2,5 +2,5 @@
 require 'global.php';
 
 foreach($obj_store->fetchAll() as $obj_book) {
-    echo "Temperature: {$obj_book->0}, Recorded: {$obj_book->recorded} <br />", PHP_EOL;
+    echo "Temperature: {$obj_book['0']}, Recorded: {$obj_book->recorded} <br />", PHP_EOL;
 }
