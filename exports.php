7f101f3e3215dbafed50e8585785a8dbe9c1afa8
comp500 <comp500@users.noreply.github.com>
2017-06-15 18:30:05 +0100

    fancy download system

--- exports.php
+++ exports.php
@@ -1,92 +1,40 @@
 <?php
 require_once 'global.php';
 
-function outputCSV($data, $titles) {
-	$titlesLength = count($titles);
-	echo "Time";
-	for ($i = 0; $i < $titlesLength; $i++) {
-		echo "," . $titles[$i];
-	}
-	echo PHP_EOL;
-	foreach($data as $obj_ent) {
-		echo $obj_ent['recorded'];
-		foreach ($obj_ent as $key => $value) {
-			if ($key == 'recorded') {
-				// ignore
-			} else {
-				echo "," . $value;
-			}
-		}
-		echo PHP_EOL;
-	}
-}
-
-function outputJSON($data, $config) {
-	$output = [
-		"metadata" => [],
-		"data" => $data
-	];
-	foreach($config as $key => $value) {
-		$output["metadata"][] = [
-			"unit" => $value["unit"],
-			"measurement" => $value["measurement"],
-			"min" => $value["graphMin"],
-			"max" => $value["graphMax"],
-			"sensorID" => $key
-		];
-	}
-	echo json_encode($output);
-}
-
 function processCaptcha() {
 	// no-op for now
 	return true;
 }
 
-function queryData($obj_store, $ascending) {
-	$dataArray = [];
-	if ($ascending) {
-		$entityArray = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded ASC");
-	} else {
-		$entityArray = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded DESC");
-	}
-	foreach($entityArray as $obj_ent) {
-		array_push($dataArray, $obj_ent->getData());
-	}
-	return $dataArray;
-}
-
-function validateDates() {
+function validate() {
 	return true;
 }
 
 if (!empty($_POST['g-recaptcha-response'])) {
 	if (processCaptcha()) {
-		if (validateDates()) {
-			if ($_POST['sort'] == "asc") {
-				$data = queryData($obj_store, true);
-			} else {
-				$data = queryData($obj_store, false);
-			}
-			$titles = [];
-			foreach($config as $key => $value) {
-				$titles[] = $value["measurement"];
-			}
-			if ($_POST['output-format'] == 'html') {
-				$smarty->assign('data', $data);
-				$smarty->assign('dataLength', count($data));
-				$smarty->assign('titles', $titles);
-				$smarty->display('exporthtml.tpl');
-			} else if ($_POST['output-format'] == 'json') {
-				outputJSON($data, $config);
-			} else {
-				outputCSV($data, $titles); // default to csv output
-			}
+		if (validate()) {
+			$export_store = new GDS\Store('Exports'); // TODO: cron job to clear old
+			/*$export_store->fetchOne("SELECT * FROM Exports WHERE ascending = @asc AND output-format = @output", [
+				'asc' => ($_POST['sort'] == 'asc'),
+				'output' => $_POST['output-format']
+			]);*/
+			$id = uniqid();
+			$export_ent = $obj_book_store->createEntity([
+				'id' => $id,
+				'time' => new DateTime(),
+				'asc' => ($_POST['sort'] == 'asc'),
+				'output' => $_POST['output-format']
+			]);
+			$export_store->insert($export_ent);
+			header('Location: /exports/download.' . $_POST['output-format'] . '?q=' . $id);
+			exit;
 		} else {
-			// redirect to error page?
+			header('Location: /exports');
+			exit;
 		}
 	} else {
-		// redirect to error page?
+		header('Location: /exports');
+		exit;
 	}
 } else {
 	$smarty->display('exports.tpl');
---
+++ exportsdownload.php
@@ -0,0 +1,96 @@
+<?php
+require_once 'global.php';
+
+function outputCSV($data, $titles) {
+	$titlesLength = count($titles);
+	echo "Time";
+	for ($i = 0; $i < $titlesLength; $i++) {
+		echo "," . $titles[$i];
+	}
+	echo PHP_EOL;
+	foreach($data as $obj_ent) {
+		echo $obj_ent['recorded'];
+		foreach ($obj_ent as $key => $value) {
+			if ($key == 'recorded') {
+				// ignore
+			} else {
+				echo "," . $value;
+			}
+		}
+		echo PHP_EOL;
+	}
+}
+
+function outputJSON($data, $config) {
+	$output = [
+		"metadata" => [],
+		"data" => $data
+	];
+	foreach($config as $key => $value) {
+		$output["metadata"][] = [
+			"unit" => $value["unit"],
+			"measurement" => $value["measurement"],
+			"min" => $value["graphMin"],
+			"max" => $value["graphMax"],
+			"sensorID" => $key
+		];
+	}
+	echo json_encode($output);
+}
+
+function processCaptcha() {
+	// no-op for now
+	return true;
+}
+
+function queryData($obj_store, $ascending) {
+	$dataArray = [];
+	if ($ascending) {
+		$entityArray = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded ASC");
+	} else {
+		$entityArray = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded DESC");
+	}
+	foreach($entityArray as $obj_ent) {
+		array_push($dataArray, $obj_ent->getData());
+	}
+	return $dataArray;
+}
+
+function validateDates() {
+	return true;
+}
+
+if (!empty($_GET['q'])) {
+	$export_store = new GDS\Store('Exports');
+	$export = $export_store->fetchOne("SELECT * FROM Exports WHERE id = @id", [
+		'id' => $_GET['q']
+	]);
+	
+	if ($export == NULL) {
+		header('Location: /exports');
+		exit;
+	}
+	
+	if ($export['sort'] == "asc") {
+		$data = queryData($obj_store, true);
+	} else {
+		$data = queryData($obj_store, false);
+	}
+	$titles = [];
+	foreach($config as $key => $value) {
+		$titles[] = $value["measurement"];
+	}
+	if ($export['output'] == 'html') {
+		$smarty->assign('data', $data);
+		$smarty->assign('dataLength', count($data));
+		$smarty->assign('titles', $titles);
+		$smarty->display('exporthtml.tpl');
+	} else if ($export['output'] == 'json') {
+		outputJSON($data, $config);
+	} else {
+		outputCSV($data, $titles); // default to csv output
+	}
+} else {
+	header('Location: /exports');
+	exit;
+}
