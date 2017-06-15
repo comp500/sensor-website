c24ec6f94c3cc8f160c569eb8dece016b86cbaed
comp500 <comp500@users.noreply.github.com>
2017-06-15 16:32:38 +0100

    fix csv export

--- exports.php
+++ exports.php
@@ -2,8 +2,18 @@
 require_once 'global.php';
 
 function outputCSV($data, $titles) {
+	echo "Time";
+	$titlesLength = count($titles);
+	for ($i = 0; $i < $titlesLength; $i++) {
+		echo "," . $titles[i];
+	}
+	echo PHP_EOL;
 	foreach($data as $obj_ent) {
-		echo "Temperature: {$obj_ent['0']}, Recorded: {$obj_ent['recorded']} <br />", PHP_EOL;
+		echo $obj_ent['recorded'];
+		for ($i = 0; $i < $titlesLength; $i++) {
+			echo "," . $obj_ent[(string)$i];
+		}
+		echo PHP_EOL;
 	}
 }
 
@@ -19,7 +29,7 @@ function queryData($obj_store, $ascending) {
 	} else {
 		$entityArray = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded DESC");
 	}
-	foreach($entityArray as $obj_ent) { // modify based on POST params
+	foreach($entityArray as $obj_ent) {
 		array_push($dataArray, $obj_ent->getData());
 	}
 	return $dataArray;
