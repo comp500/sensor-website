cc444641e720a83c5d609af55bb7f0723f35d489
comp500 <comp500@users.noreply.github.com>
2017-09-13 18:36:41 +0100

    Use live data (TODO: fix measurementTime?)

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 require_once 'global.php';
 
+function queryData($obj_store) {
+	$data = $obj_store->fetchAll("SELECT * FROM Measurement ORDER BY recorded DESC LIMIT 1");
+	$dataArray = [];
+	foreach($data as $obj_ent) {
+		array_push($dataArray, $obj_ent->getData());
+	}
+	return array_reverse($dataArray);
+}
+
+$data = queryData($obj_store);
 $templateData = [];
 foreach($config as $key => $value) {
+	$liveval = 0;
+	if (isset($data[$key])) {
+		$liveval = $data[$key];
+	}
 	$sensor = [
-		"value" => $latest[$key],
+		"value" => $liveval,
 		"unit" => $value["unit"],
 		"measurement" => $value["measurement"],
 		"location" => $value["location"],
