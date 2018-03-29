4ceecb248ca4509371a67a55932a2e46c3daf780
comp500 <comp500@users.noreply.github.com>
2018-03-29 19:56:51 +0100

    Better chart colouring

--- static/graphloader.js
+++ static/graphloader.js
@@ -1,4 +1,4 @@
-window.chartColors = { // replace with colours in code?
+/*window.chartColors = { // replace with colours in code?
 	red: 'rgb(255, 99, 132)',
 	orange: 'rgb(255, 159, 64)',
 	yellow: 'rgb(255, 205, 86)',
@@ -6,7 +6,18 @@ window.chartColors = { // replace with colours in code?
 	blue: 'rgb(54, 162, 235)',
 	purple: 'rgb(153, 102, 255)',
 	grey: 'rgb(231,233,237)'
-};
+};*/
+
+window.chartColors = [
+	'rgb(255, 99, 132)',
+	'rgb(255, 159, 64)',
+	'rgb(255, 205, 86)',
+	'rgb(75, 192, 192)',
+	'rgb(54, 162, 235)',
+	'rgb(153, 102, 255)',
+	'rgb(231,233,237)'
+];
+
 
 window.addEventListener("load", function(event) {
 	var currentSeconds = parseInt(document.getElementsByClassName("measurementTime")[0].innerText);
@@ -28,8 +39,8 @@ window.addEventListener("load", function(event) {
 					labels: ["-195", "-190", "-185", "-180", "-175", "-170", "-165", "-160", "-155", "-150", "-145", "-140", "-135", "-130", "-125", "-120", "-115", "-110", "-105", "-100", "-95", "-90", "-85", "-80", "-75", "-70", "-65", "-60", "-55", "-50", "-45", "-40", "-35", "-30", "-25", "-20", "-15", "-10", "-5", "0"],
 					datasets: [{
 						label: meta.measurement + " (" + meta.unit + ")",
-						backgroundColor: window.chartColors.red,
-						borderColor: window.chartColors.red,
+						backgroundColor: window.chartColors[i],
+						borderColor: window.chartColors[i],
 						data: ajaxdata.values[meta.sensorID],
 						fill: false
 					}]
